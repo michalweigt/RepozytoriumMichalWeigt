31a4672315f18cf0dc12bacd83280bb166f88dfa
michalweigt <michalweigt2@gmail.com>
2023-10-03 10:19:59 +0200

    dodano lub zmieniono rzecz

--- PHP lekcje/PHP 21 data.php
+++ PHP lekcje/PHP 21 data.php
@@ -4,3 +4,17 @@
     echo "Today is " . date( "d-m-Y") . "<br>";
     echo "Today is " . date( "l") . "<br>";
 ?>
+
+<?php
+    $czas = mktime( 11,14,54,8,12,2014 );
+    echo "Created date is " . date( "d.m.Y h:i:sa", $czas);
+    $now = mktime(21,37);
+    echo "<br> teraz: " . date( "d.m.Y h:i:sa", $now);
+?>
+
+<?php
+$date = date('m/d/Y');
+echo $date;
+echo "<br>";
+echo date("m/d/Y", strtotime("+14 day"));
+?>
